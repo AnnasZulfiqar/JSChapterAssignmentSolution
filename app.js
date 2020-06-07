3fafd647314c0495f8a0da488f46815f1b25dc51
AnnasZulfiqar <m.annaszulfiqar@gmail.com>
2020-06-07 06:47:26 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,65 @@
+//---------Chapter1-------
+
+// 1. Write a script to greet your website visitor using JS alert
+// box
+
+//alert ('Hi Dear User, Thank you for visiting my website');
+
+// 2. Write a script to display following message on your web
+// page:
+
+//alert("Error!! Please Enter a Valid Password");
+
+// 3. Write a script to display following message on your web
+// page: (Hint : Use line break)
+
+// var msg1 = 'Welcome to JS Land';
+// var msg2 = 'Happy Coding';
+
+// var fullMsg = msg1 + '\r\n' + msg2;
+
+// alert (fullMsg);
+
+// 4. Write a script to display following messages in sequence:
+
+// var msg1 = 'Welcome to JS Land';
+// var msg2 = 'Happy Coding';
+
+// alert (msg1);
+// alert (msg2);
+
+// 5. Generate the following message through browser’s
+// developer console:
+
+//console.log("Hello, I can run JS through my web browser's console");
+
+//6.Make use of alerts in your new/existing HTML & CSS project.
+
+//---------Chapter2-------
+
+// 1. Declare a variable called username.
+
+//var username;
+
+// 2. Declare a variable called myName & assign to it a string
+// that represents your Full Name.
+
+// var myName = 'Muhammad Annas Zulfiqar';
+
+// 3. Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box.
+
+// var msg = 'Hello World';
+// alert (msg);
+
+// 4. Write a script to save student’s bio data in JS variables and
+// show the data in alert boxes.
+
+// var name = 'Name: Muhammad Annas Zulfiqar';
+// var fname = 'Father Name: Zulfiqar';
+// var Rollnumber = 'Roll#: WM7343';
+
+// var bioData = name + '\r\n' + fname + '\r\n' + Rollnumber;
+// alert (bioData);
