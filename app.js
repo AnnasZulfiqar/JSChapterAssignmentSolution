3e420c0835a6a5316caf0d3abda69d6e6bd80b5b
AnnasZulfiqar <m.annaszulfiqar@gmail.com>
2020-06-07 16:51:04 +0500

    first push

--- app.js
+++ app.js
@@ -63,3 +63,90 @@
 
 // var bioData = name + '\r\n' + fname + '\r\n' + Rollnumber;
 // alert (bioData);
+
+var name1 = 'Annas \nAnna \nAnn \nAn \nA';
+alert (name1);
+
+var email = 'm.annaszulfiqar@Gmail.com';
+alert ('My email address is ...' + email);
+
+var book = 'A smarter way to Learn Javascript';
+alert ('I am trying to learn from the book ' + book);
+
+document.write ('YEAH!!!! I can write Html Content through Javascript');
+
+var symbol = '▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬';
+
+alert (symbol);
+
+//chapter 03 task
+
+var age = 22;
+alert ('I am ' + age + ' years old ');
+
+var visit = 23;
+alert ('You have visited this site ' + visit + ' times');
+
+var birthyear = 1996;
+document.write ('My birth year is ' + birthyear);
+document.write ('<br>');
+document.write ('\n Data type of my declared variable is' + typeof birthyear);
+
+var visitor_name = 'Furqan';
+var product_title = 'Trousers';
+var Quantity = 25;
+document.write (
+  visitor_name +
+    ' Ordered ' +
+    Quantity +
+    product_title +
+    ' on XYZ clothing Store '
+);
+
+//chap 04 task
+
+// 3 variables in one statement.
+
+var First_name, middle_name, Last_name;
+
+//5 legal variable names.
+
+var name;
+
+var First_name;
+
+var $my_1stVariable;
+
+var lastName;
+
+var price1;
+
+//5 illegal variable names
+
+// var first name;
+
+// var 1age;
+
+// var #name;
+
+// var while;
+
+// var 1stcustomer;
+
+document.write ('<h1>' + 'Rules for naming JS variables' + '</h1>');
+
+document.write (
+  'Variable names can only contain alphabet and numbers(!After any alphabet), $ and _. For example: $my_1stVariable.' +
+    '<br/>'
+);
+
+document.write (
+  'Variable must begin with a letter, $ or _. For example: $name, _name or name' +
+    '<br/>'
+);
+
+document.write (' Variable names are case sensitive' + '<br/>');
+
+document.write (
+  'Variable names should not be JS keywords like if while or do' + '<br/>'
+);
